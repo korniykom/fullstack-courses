6446a9564e1aa430d47bc6645951b703e393f17b
Max <korniykom@gmail.com>
2025-01-29 22:01:58 +0200

    NodeJS: add server for Node-Farm

--- NodeJS/Node-Farm/index.js
+++ NodeJS/Node-Farm/index.js
@@ -1,21 +1,46 @@
 import fs from "fs";
+import http from "http";
+import url from "url";
+
+/*FILES*/
 
 // const helloWorld = "Hello World!";
 // console.log(helloWorld);
 
-//Read file synchronously
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+// //Read file synchronously
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
 
-//Write file synchronously
-const textOut = `This is what we know about avocado ${textIn}. Created on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File written!");
+// //Write file synchronously
+// const textOut = `This is what we know about avocado ${textIn}. Created on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written!");
 
-//Read file asynchronously
-fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-  fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data) => {
-    console.log(data);
-  });
+// //Read file asynchronously
+// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+//   fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data) => {
+//     console.log(data);
+//   });
+// });
+// console.log("Will read the file!");
+
+/*SERVER*/
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the overview");
+  } else if (pathName === "/product") {
+    res.end("This is the product");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
 });
-console.log("Will read the file!");
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
