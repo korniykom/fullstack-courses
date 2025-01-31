b45ac731ef6890ebeacce7da87cc567a1a9e8cad
Max <korniykom@gmail.com>
2025-01-31 23:15:54 +0200

    NodeJS: exported replateTemplate function into seperate module

--- NodeJS/Node-Farm/index.js
+++ NodeJS/Node-Farm/index.js
@@ -2,6 +2,7 @@ import fs from "fs";
 import http from "http";
 import url, { fileURLToPath } from "url";
 import path from "path";
+import replaceTemplate from "./modules/replaceTemplate.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -29,23 +30,6 @@ const __dirname = path.dirname(__filename);
 // console.log("Will read the file!");
 
 /*SERVER*/
-
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-  return output;
-};
-
 const tempOverview = fs
   .readFileSync(`${__dirname}/templates/template-overview.html`)
   .toString();
