853804953c878db167052b626255ebb5524f97e8
Max <korniykom@gmail.com>
2025-02-01 13:11:43 +0200

    JS: complete Assigment-1

---
+++ JS/JavaScript Fundamentals/Assigment1-Value-and-Variables.js
@@ -0,0 +1,8 @@
+let country, continent, population;
+country = "Ukraine";
+continent = "Europe";
+population = 38_507_467;
+
+console.log(country);
+console.log(continent);
+console.log(population);
