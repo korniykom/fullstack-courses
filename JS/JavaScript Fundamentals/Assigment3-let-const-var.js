e424e2d7cb181edd7b544077a77144282a30288d
Max <korniykom@gmail.com>
2025-02-01 15:40:48 +0200

    JS: complete Assigment-3

---
+++ JS/JavaScript Fundamentals/Assigment3-let-const-var.js
@@ -0,0 +1,13 @@
+// Note: avoid using var
+
+const country = "Ukraine";
+const continent = "Europe";
+const population = 38_507_467;
+const isIsland = false;
+const language = "ukrainian";
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof language);
+console.log(typeof isIsland);
