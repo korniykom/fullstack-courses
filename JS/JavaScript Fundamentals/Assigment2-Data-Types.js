179be2eebbbc092a33ecf7f692508f176b5eb06b
Max <korniykom@gmail.com>
2025-02-01 15:31:38 +0200

    JS: complete Assigment-2

---
+++ JS/JavaScript Fundamentals/Assigment2-Data-Types.js
@@ -0,0 +1,12 @@
+let country, continent, population, isIsland, language;
+
+country = "Ukraine";
+continent = "Europe";
+population = 38_507_467;
+isIsland = false;
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof language);
+console.log(typeof isIsland);
