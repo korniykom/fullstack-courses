b0c72c5c919590bb4737727c0adb56c14f9e95d4
Max <korniykom@gmail.com>
2025-02-02 15:53:22 +0200

    JS: complete Assigment-4

---
+++ JS/JavaScript Fundamentals/Assigment4-Basic-Operators.js
@@ -0,0 +1,27 @@
+const country = "Ukraine";
+const continent = "Europe";
+let population = 38_507_467;
+const isIsland = false;
+const language = "ukrainian";
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof language);
+console.log(typeof isIsland);
+
+console.log(population / 2);
+console.log(++population);
+console.log(6_000_000 > population);
+console.log(33_000_000 > population);
+
+const description =
+  country +
+  " is in " +
+  continent +
+  ", and its " +
+  population +
+  " people speak " +
+  language;
+
+console.log(description);
