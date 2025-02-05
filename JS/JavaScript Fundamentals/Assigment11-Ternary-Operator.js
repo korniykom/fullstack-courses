9777827000d70510c19ce4d017c7d4611e6e4d3e
Max <korniykom@gmail.com>
2025-02-05 15:10:24 +0200

    JS: complete Assigment-11

---
+++ JS/JavaScript Fundamentals/Assigment11-Ternary-Operator.js
@@ -0,0 +1,9 @@
+const country = "Ukraine";
+const continent = "Europe";
+let population = 38_507_467;
+const isIsland = false;
+const language = "ukrainian";
+
+population > 33_000_000
+  ? console.log(`${country}'s population is above average`)
+  : console.log(`${country}'s population is below average`);
