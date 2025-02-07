7659552b1b63b27333af6167595d84305868bd01
Max <korniykom@gmail.com>
2025-02-07 20:01:59 +0200

    JS: complete Assigment-15

---
+++ JS/JavaScript Fundamentals/Assigment15-Arrow-Functions.js
@@ -0,0 +1,9 @@
+// Recreate the last assignment, but this time create an arrow function called percentageOfWorld3.
+
+const percentageOfWorld3 = (population) => (7900 / population) * 100;
+
+const percPortugal3 = percentageOfWorld3(10);
+const percChina3 = percentageOfWorld3(1441);
+const percUSA3 = percentageOfWorld3(332);
+
+console.log(percPortugal3, percChina3, percUSA3);
