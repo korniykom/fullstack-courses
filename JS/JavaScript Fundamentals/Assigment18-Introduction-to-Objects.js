2eae1a1c62b3fd57751f5ef274de5b8dd4bfdb78
Max <korniykom@gmail.com>
2025-02-13 17:29:42 +0200

    JS: complete Assigment-18

---
+++ JS/JavaScript Fundamentals/Assigment18-Introduction-to-Objects.js
@@ -0,0 +1,19 @@
+// Create an object called myCountry for a country of your choice,
+// containing properties country, capital, language, population and
+//  neighbours (an array like we used in previous assignments).
+
+const myCountry = {
+  country: "Ukraine",
+  capital: "Kyiv",
+  language: "ukrainian",
+  population: 37_000_000,
+  neightbours: [
+    "Poland",
+    "Slovakia",
+    "Hungary",
+    "Romania",
+    "Moldova",
+    "Russia",
+    "Belarus",
+  ],
+};
