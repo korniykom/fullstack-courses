d5ea4cf4c8655478c2ca3c514bf291729006f49d
Max <korniykom@gmail.com>
2025-02-14 21:23:38 +0200

    TS: add expmale of array types in TS

--- TS/TypeScript-Fundamentals/object-type.ts
+++ TS/TypeScript-Fundamentals/object-type.ts
@@ -1,9 +1,14 @@
 const person: {
   name: string;
   age: number;
+  hobies: string[];
 } = {
   name: "Max",
   age: 19,
+  hobies: ["sports", "cooking"],
 };
 
 console.log(person.name);
+for (const hobby of person.hobies) {
+  console.log(hobby.toUpperCase());
+}
