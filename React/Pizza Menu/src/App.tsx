bc0bbcd7aa7947b5e6010125389acd65cfd192ad
Max <korniykom@gmail.com>
2025-02-14 16:37:24 +0200

    React: create structure for Pizza Menu project

---
+++ React/Pizza Menu/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Menu />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ React/Pizza Menu/src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <>
+      <div className="header">
+        <h1 className="header__text">Fast React Pizza Co.</h1>
+      </div>
+    </>
+  );
+}
