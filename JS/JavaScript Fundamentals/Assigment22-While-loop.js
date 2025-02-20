805a371bf420d26ee98425008869dd8d343eaa39
Max <korniykom@gmail.com>
2025-02-20 18:12:29 +0200

    JS: complete Assigment 22

---
+++ JS/JavaScript Fundamentals/Assigment22-While-loop.js
@@ -0,0 +1,9 @@
+// Recreate the challenge from the lecture Looping Arrays, Breaking and Continuing, but this time using a while loop (call the array percentages3).
+
+// Reflect on what solution you like better for this task: the for loop or the while loop?
+
+let voterNumber = 1;
+while (voterNumber <= 50) {
+  console.log(`Voter ${voterNumber} is currently voting`);
+  voterNumber++;
+}
